49e36d12d8b611fb936624e35474fc5193ef01e4
Eriic7 <125944053+Eriic7@users.noreply.github.com>
2023-03-01 16:12:22 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,54 @@
+
+document.getElementById("boton_color").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#D7EC78";
+});
+
+document.getElementById("boton_color").addEventListener("click", function() {
+    document.body.style.color = "#B478EC";
+});
+
+document.getElementById("boton_color1").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#996AE3";
+});
+
+document.getElementById("boton_color1").addEventListener("click", function() {
+    document.body.style.color = "#1460AB";
+});
+
+document.getElementById("boton_color2").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#0B0F09";
+});
+
+document.getElementById("boton_color2").addEventListener("click", function() {
+    document.body.style.color = "#84230B";
+});
+
+document.getElementById("boton_color3").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#251F8E";
+});
+
+document.getElementById("boton_color3").addEventListener("click", function() {
+    document.body.style.color = "#68FA71";
+});
+
+document.getElementById("boton_color4").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#19C9B4";
+});
+
+document.getElementById("boton_color4").addEventListener("click", function() {
+    document.body.style.color = "#1A0CC8";
+});
+
+document.getElementById("boton_default").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#E0ECF4";
+    });
+
+document.getElementById("boton_default").addEventListener("click", function() {
+    document.body.style.color= "#000000";
+    });
+
+document.getElementById("boton_ocultar").addEventListener("click", function() {
+document.getElementById("demo").style.display = "none";
+});
+
+
